235b846bfc8126017d9e032497386467620a09e3
David Papamichael <davidpapamichael@gmail.com>
2020-05-04 12:45:31 +0100

    weather generator function added

--- jasmine-3.5.0/spec/weatherSpec.js
+++ jasmine-3.5.0/spec/weatherSpec.js
@@ -2,4 +2,20 @@
 
 describe("Weather", function () {
   let weather;
+
+  beforeEach(function () {
+    weather = new Weather();
+  });
+
+  describe("generate weather", function () {
+    it("stormy condition - returns true", function () {
+      spyOn(weather, "generateWeather").and.returnValue(true);
+      expect(weather.generateWeather()).toBeTruthy();
+    });
+
+    it("normal condition - returns false", function () {
+      spyOn(weather, "generateWeather").and.returnValue(false);
+      expect(weather.generateWeather()).toBeFalsy();
+    });
+  });
 });
--- jasmine-3.5.0/src/weather.js
+++ jasmine-3.5.0/src/weather.js
@@ -1,3 +1,7 @@
 class Weather {
   constructor() {}
+
+  generateWeather = () => {
+    return Math.random() < 0.5;
+  };
 }
